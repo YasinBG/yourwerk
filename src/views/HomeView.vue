fc4a1e0b6eabf2471de3323bfdc90bed2fa012a2
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-12-18 15:44:42 +0300

    textcard olusturuldu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,19 @@
 
   <CardOne></CardOne>
   <ImgCard></ImgCard>
+  <TextCard></TextCard>
 </div>
 </template>
 
 <script>
 import CardOne from "../components/CardOne.vue"
 import ImgCard from "../components/ImgCard.vue"
+import TextCard from "@/components/TextCard.vue";
 export default {
 components:{
   CardOne,
-  ImgCard
+  ImgCard,
+  TextCard
 }
 };
 
