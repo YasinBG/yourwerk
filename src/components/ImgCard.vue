7a8e23c1f008b86041f0a3d701aee5a5f243739a
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-12-18 15:20:56 +0300

    card img olusturuldu

---
+++ src/components/ImgCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="text-gray-600">
+    <div class="container px-5 py-12 mx-auto w-1/4">
+        <div class="flex flex-wrap m-4">
+            <div class="m-4 md:1/3">
+                <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                    <img src="https://images.unsplash.com/photo-1604594849809-dfedbc827105?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="lg:h-48 md:h-36 w-full object-cover object-center">
+                    <div class="p-6">
+                        <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1 uppercase">
+                            Kategori
+                        </h2>
+
+                        <h1 class="title-font text-xl font-medium text-gray-00 mb-3 uppercase">
+                            Title
+
+                        </h1>
+
+                        <p class="leading-relaxed mb-3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ab magnam eos tenetur cumque laboriosam distinctio architecto odit veritatis. Ea minima error dolorem recusandae aperiam et blanditiis qui inventore magni.
+
+                        </p>
+                        <div>
+                            <button>daha fazla bilgi icin tiklayiniz</button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,17 @@
 <div>test
 
   <CardOne></CardOne>
+  <ImgCard></ImgCard>
 </div>
 </template>
 
 <script>
 import CardOne from "../components/CardOne.vue"
+import ImgCard from "../components/ImgCard.vue"
 export default {
 components:{
-  CardOne
+  CardOne,
+  ImgCard
 }
 };
 
