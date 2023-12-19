cd6d302662a98907e9c6c408464ef8e52dd84ec6
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-12-19 18:45:55 +0300

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,14 @@
 <template>
 <div>test
 
-  <CardOne></CardOne>
-  <ImgCard></ImgCard>
-  <TextCard></TextCard>
+  
 </div>
 </template>
 
 <script>
-import CardOne from "../components/CardOne.vue"
-import ImgCard from "../components/ImgCard.vue"
-import TextCard from "@/components/TextCard.vue";
 export default {
 components:{
-  CardOne,
-  ImgCard,
-  TextCard
+  
 }
 };
 
