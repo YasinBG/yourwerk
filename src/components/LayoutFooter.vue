5253924b5e73d7547fd04d4b9c3a7cdd5a551fb6
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-12-25 12:24:38 +0300

    update

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="dark:bg-mkPrimary text-mkPrimary dark:text-mkWhite selection:bg-mkPurple selection:text-mkWhite">
+  <div class="dark:bg-mkPrimary text-mkPrimary dark:text-mkWhite selection:bg-mkPurple selection:text-mkWhite border-t-2 flex">
+
+    <span
+            v-motion
+            :initial="{ opacity: 0, y: -100 }"
+            :enter="{
+              opacity: 1,
+              y: 0,
+              scale: 1,
+              transition: { duration: 700 },
+            }"
+            :variants="{ custom: { scale: 2 } }"
+            :hovered="{ scale: 1.2 }"
+            class="logo mt-12 ml-24 flex items-center text-3xl border-b-2 border-b-mkPurple cursor-pointer"
+          >
+          Multi<span class="italic font-serif dark:text-mkPurple text-mkGray"
+              >funct</span
+            ><span class="font-bold">ional</span>
+          </span>
      
-    <ul class="nav-list flex items-center space-x-12 font-semibold uppercase">
+    <ul class="nav-list mt-12 flex items-center space-x-12 font-semibold uppercase ml-40">
       <li  v-for="menu in menus" :key="menu"    
               class="hover:opacity-70 focus:opacity-70 transition-all duration-200 ease-linear cursor-pointer"
             >
