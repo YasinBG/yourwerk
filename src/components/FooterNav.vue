e6c732243d9c3097e0d9db10870c23689d756768
YasinBG <116117219+YasinBG@users.noreply.github.com>
2024-01-15 12:04:20 +0300

    update

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -3,7 +3,7 @@
      
      <ul class="nav-list flex items-center space-x-12 font-semibold uppercase  ">
        <li  v-for="menu in menus" :key="menu"    
-               class="hover:opacity-70 focus:opacity-70 transition-all duration-200 ease-linear cursor-pointer"
+               class="hover:opacity-70 focus:opacity-70 transition-all duration-200 ease-linear cursor-pointer uppercase"
              >
              <a href="#">{{ menu.name }}</a>
              </li>
@@ -27,14 +27,22 @@ export default {
         name:'home', detail:'',
       },
       {
-        name:'Docs',  detail:'',
+        name:'whitepaper',  detail:'',
       },
       {
-        name:'about',  detail:'',
+        name:'home to buy',  detail:'',
       },
       {
         name:'contact',  detail:'',
+      },
+       {
+      name:'buy',  detail:'',
+      },
+     
+      {
+      name:'privacy',  detail:'',
       }
+     
      
     ])
       return{
