a9247216cae5c0aa18ea03de70476925ddcee203
Mustafa Kemal Karadeniz <108372741+M-kemal@users.noreply.github.com>
2024-01-15 17:10:45 +0300

    update

--- src/views/PrivacyView.vue
+++ src/views/PrivacyView.vue
@@ -1,94 +1,232 @@
 <template>
-  <div class="bgnew text-white">
-  <LayoutHeader></LayoutHeader>
-</div>
-  <div class="text-white">
-    <h2>Privacy Policy for WERK Token WEBSITE</h2>
-    <p>Effective Date: 01/10/2024</p>
-    <h4>Introduction:</h4>
-    <p>This Privacy Policy delineates our approach to managing your information in relation to the WERK Token ecosystem, emphasizing our commitment to privacy in the rapidly evolving Web3 landscape.</p>
+  <div
+    class="dark:bg-mkPrimary text-mkPrimary dark:text-mkWhite selection:bg-mkPurple selection:text-mkWhite flex flex-col items-center"
+    :class="{ 'bgnew': isDarkMode }"
+  >
+    <LayoutHeader></LayoutHeader>
+    <div class="text-white bg-transparent">
+      <h2>Privacy Policy for WERK Token WEBSITE</h2>
+      <p>Effective Date: 01/10/2024</p>
+      <h4>Introduction:</h4>
+      <p>
+        This Privacy Policy delineates our approach to managing your information
+        in relation to the WERK Token ecosystem, emphasizing our commitment to
+        privacy in the rapidly evolving Web3 landscape.
+      </p>
 
-    <h3>1. Information Collection and Utilization:</h3>
-    <ul>
-        <li>Personal Data: Essential for individualized service, we may collect personal information such as names, email addresses, and contact details.</li>
-        <li>Blockchain Data: Central to our operations, we handle public blockchain data, including transaction histories and wallet addresses. This data is public by nature and integral to our services.
-</li>
+      <h3>1. Information Collection and Utilization:</h3>
+      <ul>
+        <li>
+          Personal Data: Essential for individualized service, we may collect
+          personal information such as names, email addresses, and contact
+          details.
+        </li>
+        <li>
+          Blockchain Data: Central to our operations, we handle public
+          blockchain data, including transaction histories and wallet addresses.
+          This data is public by nature and integral to our services.
+        </li>
 
-<li>
-    Usage Data: We may track interaction with the WERK Token platform to enhance service quality and user experience.
+        <li>
+          Usage Data: We may track interaction with the WERK Token platform to
+          enhance service quality and user experience.
+        </li>
+      </ul>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
 
-</li>
-
-
-    </ul>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe cumque maiores incidunt laboriosam pariatur possimus quis tempora labore? Veritatis, praesentium quisquam!lorem10
-      
-    </p>
-    
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ratione
+        consequatur voluptate cupiditate, ipsa aliquam, error perspiciatis saepe
+        cumque maiores incidunt laboriosam pariatur possimus quis tempora
+        labore? Veritatis, praesentium quisquam!lorem10
+      </p>
+    </div>
+    <LayoutFooter />
   </div>
 </template>
 
 <script>
-
-import LayoutHeader from '../components/LayoutHeader.vue'
+import { inject } from "vue";
+import LayoutHeader from "../components/LayoutHeader.vue";
+import LayoutFooter from "@/components/LayoutFooter.vue";
 export default {
- components:{
-  LayoutHeader
- }
-}
+  components: {
+    LayoutHeader,
+    LayoutFooter,
+  },
+  setup() {
+    const isDarkMode = inject("isDarkMode");
+
+    return { isDarkMode };
+  },
+};
 </script>
 
-<style scoped>
+<style>
+.bgnew {
+  /* background: url("../assets/mkover.webp"); */
+  background: linear-gradient(
+    to right,
+    #0b2142,
+    #0d2e55,
+    #103b68,
+    #12487b,
+    #14558e
+  );
+  /* background: linear-gradient(to right, #080a10, #241b28); */
 
+  /* background: linear-gradient(to right, #080a10, #241b28, #241a28, #090b11); */
+  /* background: linear-gradient(to right, #1a182c, #241a28); */
 
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  /* background-attachment: fixed; */
+
+  /* background-image: linear-gradient(180deg, #080a10 0%, #080a1000 100%); */
+  /* transition: background 0.3s, border-radius 0.3s, opacity 0.3s; */
+}
 </style>
